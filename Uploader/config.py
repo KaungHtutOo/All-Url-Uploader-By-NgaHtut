2d5a10b14e941f4186e7f98c5a54876950fb7c43
KaungHtutOo <73459374+KaungHtutOo@users.noreply.github.com>
2022-12-04 16:30:35 +0630

    Update config.py

--- Uploader/config.py
+++ Uploader/config.py
@@ -35,21 +35,21 @@ logging.basicConfig(
 class Config(object):
     WEBHOOK = os.environ.get("BOT_TOKEN", False)
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5838279749:AAF2NNAVAN-rL9iEb_kwVNigU6IJcJomK3I")
     # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    API_ID = int(os.environ.get("API_ID", 7978114))
+    API_HASH = os.environ.get("API_HASH", "5f7839feeba133497f24acfd005ef2ec")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
 
     # file /video dpwnload location
     DOWNLOAD_LOCATION = "./DOWNLOADS"
 
-    MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "None")
+    MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "ngahtut.kho@gmail.com")
     # If deploying on vps edit the above value as example := Mega_email = "Your-Mega_email-inside-inverted-commas."
 
     # This is not necessary! Enter your mega password only if you have a mega.nz account with pro/business features.
-    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "None")
+    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "505953KhoPep$")
     # If deploying on vps edit the above value as example := Mega_password = "Your-Mega_password-inside-inverted-commas."
     # Telegram maximum file upload size
     TG_MAX_FILE_SIZE = 4194304000
@@ -64,8 +64,8 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3700
 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
-    OWNER_ID = int(os.environ.get("OWNER_ID", "12356"))
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1760280598"))
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "UrlUploaderBot")
     ADL_BOT_RQ = {}
     AUTH_USERS = list({int(x)
                       for x in os.environ.get("AUTH_USERS", "0").split()})
